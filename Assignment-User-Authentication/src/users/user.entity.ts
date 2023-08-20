e22ecddc574927fb8730cb2cb085ad228797d035
zafarsaleem1984 <87990113+zafarsaleem1984@users.noreply.github.com>
2023-08-20 23:14:45 +0500

    add information

---
+++ Assignment-User-Authentication/src/users/user.entity.ts
@@ -0,0 +1,30 @@
+// src/user/user.entity.ts
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
+
